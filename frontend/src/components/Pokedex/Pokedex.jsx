0c74025757ac19d30c4b7b6a4b880b1816a1810b
e-genie <eugenie.froustey@gmail.com>
2022-11-10 11:40:00 +0100

    design modification on pokedex page

--- frontend/src/components/Pokedex/Pokedex.jsx
+++ frontend/src/components/Pokedex/Pokedex.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import setBackgroundType from "../../tools/setBackgroundType";
+import setBackgroundName from "../../tools/setBackgroundName";
 import setFirstLogo from "../../tools/setFirstLogo";
 import setSecondLogo from "../../tools/setSecondLogo";
 import setNumber from "../../tools/setNumber";
+import setAvailableToFight from "../../tools/setAvailableToFight";
 import { bgGradLightGrey } from "../../tools/constants";
 
 function Pokedex({ pokemon }) {
@@ -12,7 +14,9 @@ function Pokedex({ pokemon }) {
       className={`${bgGradLightGrey} rounded-xl shadow-md w-40 h-44 mx-2 my-3 sm:mx-5 sm:my-5`}
     >
       <div className="flex justify-between">
-        <div className="font-Silkscreen text-sm">{pokemon.name}</div>
+        <div className="w-full rounded-tr-lg">
+          <div className="font-Silkscreen text-sm pl-1">{pokemon.name}</div>
+        </div>
         <div className={`flex rounded-tr-lg ${setBackgroundType(pokemon)}`}>
           <div className="">{setFirstLogo(pokemon)}</div>
           <div className="">{setSecondLogo(pokemon)}</div>
@@ -25,8 +29,11 @@ function Pokedex({ pokemon }) {
           alt="Pokemon"
         />
       </div>
-      <div className="flex justify-end font-Rajdhani text-sm mr-2 mb-1">
-        {setNumber(pokemon)}
+      <div className="flex justify-between pl-2">
+        <div className="h-5 w-5">{setAvailableToFight(pokemon)}</div>
+        <div className="flex justify-end font-Rajdhani text-sm mr-2 mb-1">
+          {setNumber(pokemon)}
+        </div>
       </div>
     </div>
   );
