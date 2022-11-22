caa1fe69041c1edf277d46a761787825300885a4
MaximeB29 <belin.maxime9@gmail.com>
2022-11-22 16:53:00 +0100

    improvements, fix and responsive

--- frontend/src/components/Home/About.jsx
+++ frontend/src/components/Home/About.jsx
@@ -3,13 +3,21 @@ import React from "react";
 function About() {
   return (
     <div className="bg-white dark:bg-[#AFAFAF]">
-      <div className="flex m-5 space-x-7 py-6 text-center items-center text-red-700 dark:text-white font-Rajdhani">
+      <div className="flex m-5 sm:mx-20 space-x-7 py-6 md:px-5 text-center items-center text-red-700 dark:text-white font-Rajdhani">
         <h2 className="text-xl rounded-md">About Pokewild</h2>
-        <p className="text-sm text-left lg:text-xl">
-          Welcome to the Pokewild game site! Based on the 1st generation of
-          Pokémons, you can find here a Pokédex. We also offer you to fight
+        <p className="text-sm lg:text-base md:hidden">
+          Welcome to the Pokewild game site! <br /> Based on the 1st generation
+          of Pokémons, you can find here a Pokédex. We also offer you to fight
           against a random opponent chosen by the computer to test your agility.
           Let's start now!
+        </p>
+        <p className="hidden md:visible md:flex">
+          Welcome to the Pokewild game site!
+          <br /> Based on the 1st generation of Pokémons, you can find here a
+          Pokédex. We also offer you to fight against a random opponent chosen
+          by the computer to test your experience and your agility. You can
+          choose your battle arena as well as your character. They will be able
+          to evolve as they gain experience during the fights. Let's start now!
         </p>
       </div>
     </div>
